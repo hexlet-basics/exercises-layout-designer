37e179b2eb4431c38dec70d83cea1070942b28c5
Nikita Mikhaylov <nmikhaylov92@gmail.com>
2023-01-30 14:34:34 +0300

    add exercise for flex-direction lesson

---
+++ modules/70-flex/15-flex-direction/test.js
@@ -0,0 +1,9 @@
+const { test } = require('tests');
+
+test(({ query, expect }) => {
+  const flexContainer = document.querySelectorAll('.qa');
+
+  const style = getComputedStyle(flexContainer);
+  expect(style).to.have.property('display', 'flex');
+  expect(style).to.have.property('flex-direction', 'column');
+});
